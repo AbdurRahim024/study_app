f5342c4de1c7f764b25f4a4e971d41a56f6c865d
Abdurrahim <argigani24@gmail.com>
2024-01-20 14:57:43 -0500

    Added all the required modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ToDoModule } from './to-do/to-do.module';
+import { AuthenticationModule } from './authentication/authentication.module';
+import { NotebookModule } from './notebook/notebook.module';
+import { MusicModule } from './music/music.module';
+import { MusciService } from './musci/musci.service';
 
 @Module({
-  imports: [],
+  imports: [ToDoModule, AuthenticationModule, NotebookModule, MusicModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, MusciService],
 })
 export class AppModule {}
